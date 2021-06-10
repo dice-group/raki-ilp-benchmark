fa9c1c7e2be3957003f9f662ca13c8e58b9a6824
TortugaAttack <felixconrads@googlemail.com>
2021-06-10 15:29:03 +0200

    small fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 mvn clean install -Dmaven.test.skip=true
 
+cd raki-system-adapter && wget https://github.com/dice-group/Ontolearn/blob/main/embeddings.zip?raw=true -O embeddings.zip && cd ..
+cd raki-system-adapter && wget https://github.com/dice-group/Ontolearn/blob/main/pre_trained_agents.zip?raw=true -O pre_trained_agents.zip && cd ..
+
 cd raki-benchmark && docker build -t git.project-hobbit.eu:4567/raki/raki-private/raki-benchmark/rakibenchmark . && cd ..
 cd raki-datagenerator &&  docker build -t git.project-hobbit.eu:4567/raki/raki-private/raki-benchmark/rakidatagenerator . && cd ../
 cd raki-taskgenerator  && docker build -t git.project-hobbit.eu:4567/raki/raki-private/raki-benchmark/rakitaskgenerator . && cd ../
 cd raki-evaluation && docker build -t git.project-hobbit.eu:4567/raki/raki-private/raki-benchmark/rakievaluationmodule . && cd ../
-#cd raki-system-adapter && docker build -f ontolearn-dockerfile.docker -t git.project-hobbit.eu:4567/raki/raki-private/raki-systems/ontolearn . && cd ../
+cd raki-system-adapter && docker build -f ontolearn-dockerfile.docker -t git.project-hobbit.eu:4567/raki/raki-private/raki-systems/ontolearn . && cd ../
 cd raki-system-adapter && docker build -f dllearner-dockerfile.docker -t git.project-hobbit.eu:4567/raki/raki-private/raki-systems/dllearner . && cd ../
 
 
