6d121a6e7185fdc62c90c90d63ad619d83dc92cb
Jean-KOUAGOU <jeank@aims.ac.za>
2023-07-24 18:42:36 +0200

    update scripts to integrate nces

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ sudo docker build -f raki-hobbit/controller.dockerfile -t git.project-hobbit.eu:
 sudo docker build -f raki-hobbit/datagenerator.dockerfile -t git.project-hobbit.eu:4567/raki/raki-ilp-benchmark/rakidatagenerator raki-hobbit
 sudo docker build -f raki-hobbit/taskgenerator.dockerfile -t git.project-hobbit.eu:4567/raki/raki-ilp-benchmark/rakitaskgenerator raki-hobbit
 sudo docker build -f raki-hobbit/evaluation.dockerfile -t git.project-hobbit.eu:4567/raki/raki-ilp-benchmark/rakievaluationmodule raki-hobbit
+sudo docker build -f raki-system-adapter/nces-dockerfile.docker -t git.project-hobbit.eu:4567/raki/raki-ilp-benchmark-systems/nces raki-system-adapter
 sudo docker build -f raki-system-adapter/ontolearn-dockerfile.docker -t git.project-hobbit.eu:4567/raki/raki-ilp-benchmark-systems/ontolearn raki-system-adapter
 sudo docker build -f raki-system-adapter/dllearner-dockerfile.docker -t git.project-hobbit.eu:4567/raki/raki-ilp-benchmark-systems/dllearner raki-system-adapter
